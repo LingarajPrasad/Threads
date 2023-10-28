71ce101b11daf235d5bd8a4c6a2b5dd230ecfce6
Lingaraj <lingarajprasadsahu2002@gmail.com>
2023-10-28 16:13:58 +0530

    v1.1

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
 		//frontend link
-		origin: ["https://threads-three-sage.vercel.app"],
+		origin: "https://threads-three-sage.vercel.app",
 		// origin: "http://localhost:8000", 
 		
 		methods: ["GET", "POST"],
